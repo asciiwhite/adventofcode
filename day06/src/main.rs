6fa4a8c7da881d8024ad06e9da80c9b722a98227
Christian Vix <christian_vix@mentor.com>
2019-01-09 11:33:40 +0100

    simplified day06

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,14 +1,9 @@
 #![allow(non_snake_case)]
-#![allow(dead_code)]
 
 #[macro_use]
 extern crate text_io;
 
 use std::fs;
-use std::thread;
-
-const GRIDSIZE_WIDTH: usize = 360;
-const GRIDSIZE_HEIGHT: usize = 360;
 
 type Coord = (i32, i32);
 type Coords = Vec<Coord>;
@@ -20,77 +15,77 @@ struct GridCell
     id: i32,
 }
 
-type Grid = [[GridCell; GRIDSIZE_WIDTH]; GRIDSIZE_HEIGHT];
-
-fn readCoords(lines: std::str::Lines) -> Coords
+fn readCoords(lines: std::str::Lines) -> (Coords, i32, i32)
 {
-    lines.map(|line| {
+    let coords: Coords = lines.map(|line| {
         let x: i32;
         let y: i32;
         scan!(line.bytes() => "{},{}", x, y);
         (x, y)
-    }).collect()
+    }).collect();
+
+    let width  = *coords.iter().map(|(x,_)| x).max().unwrap();
+    let height = *coords.iter().map(|(_,y)| y).max().unwrap();
+
+    (coords, width, height)
 }
 
-fn manhattanDistance(coord: &Coord, x: usize, y: usize) -> i32
+fn manhattanDistance(coord: &Coord, x: i32, y: i32) -> i32
 {
-    (coord.0 - x as i32).abs() + (coord.1 - y as i32).abs()
+    (coord.0 - x).abs() + (coord.1 - y).abs()
 }
 
-fn processGrid(grid: &mut Grid, coords: &Coords)
+fn processGrid(coords: &Coords, width: i32, height: i32)
 {
+    let mut grid = vec![GridCell{ dist: std::i32::MAX, id: coords.len() as i32 }; (width * height) as usize];
+
     for (id, coord) in coords.iter().enumerate() {
-        for (y, row) in grid.iter_mut().enumerate() {
-            for (x, col) in row.iter_mut().enumerate() {
+        for y in 0..height {
+            for x in 0..width {
                 let dist = manhattanDistance(coord, x, y);
-                if dist < col.dist {
-                    col.dist = dist;
-                    col.id = id as i32;
+                let cell = &mut grid[(y * width + x) as usize];
+                if dist < cell.dist {
+                    cell.dist = dist;
+                    cell.id = id as i32;
                 }
-                else if dist == col.dist {
-                    col.id = -1;
+                else if dist == cell.dist {
+                    cell.id = -1;
                 }
             };
         };
     }
 
     let mut cellCount = vec![0i32; coords.len()];
-    for row in grid.iter() {
-        for cell in row.iter() {
+    for y in 0..height {
+        for x in 0..width {
+            let cell = grid[(y * width + x) as usize];
             if cell.id >= 0 {
                 cellCount[cell.id as usize] += 1;
             }
         }
     }
 
-    for row in grid.first() {
-        for cell in row.iter() {
-            if cell.id >= 0 { cellCount[cell.id as usize] = 0; };
-         }
+    for y in 0..height {
+        let cell = &grid[(y * width) as usize];
+        if cell.id >= 0 { cellCount[cell.id as usize] = 0; };
+        let cell = &grid[(y * width + width - 1) as usize];
+        if cell.id >= 0 { cellCount[cell.id as usize] = 0; };
     }
-    for row in grid.last() {
-        for cell in row.iter() {
-             if cell.id >= 0 { cellCount[cell.id as usize] = 0; };
-         }
-    }
-    for row in grid.iter() {
-        for cell in row.first() {
-             if cell.id >= 0 { cellCount[cell.id as usize] = 0; };
-         }
-    }
-    for row in grid.iter() {
-        for cell in row.last() {
-             if cell.id >= 0 { cellCount[cell.id as usize] = 0; };
-         }
+
+    for x in 0..width {
+        let cell = &grid[x as usize];
+        if cell.id >= 0 { cellCount[cell.id as usize] = 0; };
+        let cell = &grid[((height - 1) * width + x) as usize];
+        if cell.id >= 0 { cellCount[cell.id as usize] = 0; };
     }
     println!("size of the largest area: {}", cellCount.iter().max().unwrap());
 }
 
-fn findRegion(coords: &Coords, max_dist: i32)
+fn findArea(coords: &Coords, width: i32, height: i32, max_dist: i32)
 {
     let mut area = 0;
-    for y in 0..GRIDSIZE_HEIGHT {
-        for x in 0..GRIDSIZE_WIDTH {
+    for y in 0..height {
+        for x in 0..width {
             let dist = coords.iter().map(|coord| manhattanDistance(coord, x ,y)).sum::<i32>();
             area += if dist < max_dist { 1 } else { 0 };
         }
@@ -98,28 +93,10 @@ fn findRegion(coords: &Coords, max_dist: i32)
     println!("size of region: {}", area);
 }
 
-fn printGrid(grid: &Grid) {
-    for row in grid.iter() {
-        for col in row.iter() {
-            print!("{:>2} ", col.id);
-        }
-        println!("");
-    }
-}
-
 fn main() {
     let filename = "input.txt";
     let content = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    let coords = readCoords(content.lines());
-
-    // stack size is to small so use an separate thread with bigger stack
-    let builder = thread::Builder::new().stack_size(2 * 1024 * 1024);
-    let handler = builder.spawn(move || {
-        let mut grid: Grid = [[GridCell{ dist: std::i32::MAX, id: coords.len() as i32 }; GRIDSIZE_WIDTH]; GRIDSIZE_HEIGHT];
-        processGrid(&mut grid, &coords);
-        findRegion(&coords, 10000);
-//        printGrid(&grid);
-    }).unwrap();
-
-    handler.join().unwrap();
+    let (coords, width, height) = readCoords(content.lines());
+    processGrid(&coords, width, height);
+    findArea(&coords, width, height, 10_000);
 }
