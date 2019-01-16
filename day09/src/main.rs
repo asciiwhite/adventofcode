ff5aa5c79151549e5fc8ce2840811faa2aad764e
Christian Vix <christian_vix@mentor.com>
2019-01-16 14:30:51 +0100

    Improved day09

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -4,28 +4,23 @@ use linked_list::{Cursor, LinkedList};
 use std::time::{Instant};
 
 fn go_forward<T>(cursor: &mut Cursor<T>) {
-    (0..2).for_each(|_| {
+    for _ in 0..2 {
         while cursor.next().is_none() {
              cursor.reset();
         };
-    });
+    };
 }
 
-fn go_backwards(cursor: &mut Cursor<usize>) -> usize {
-    let mut value = 0;
-    (0..7).for_each(|_| {
-        let mut res = cursor.prev();
-        if res.is_none() { cursor.reset(); cursor.prev(); res = cursor.prev(); }
-        value = *res.unwrap();
-    });
-    value
+fn go_backwards(cursor: &mut Cursor<usize>) {
+    for _ in 0..7 {
+        if cursor.prev().is_none() { cursor.reset(); cursor.prev(); cursor.prev(); }
+    };
 }
 
 fn main() {
     let numPlayers = 416;
     let mut playerPoints = vec![0;  numPlayers];
     let numMarbles = 7161700;
-    let mut currentPlayer = 0;
     let mut circle: LinkedList<usize> = LinkedList::new();
     circle.push_back(0);
     let mut cursor = circle.cursor();
@@ -33,18 +28,16 @@ fn main() {
 
     let now = Instant::now();
 
-    (1..=numMarbles).for_each(|currentMarble| {
-        if currentMarble % 23 == 0 {
-            let removedMarble = go_backwards(&mut cursor);
-            playerPoints[currentPlayer] += currentMarble + removedMarble;
-            cursor.remove();
+    for (marble, player) in (1..=numMarbles).zip((0..numPlayers).cycle()) {
+        if marble % 23 == 0 {
+            go_backwards(&mut cursor);
+            playerPoints[player] += marble + cursor.remove().unwrap();
         }
         else {
             go_forward(&mut cursor);
-            cursor.insert(currentMarble);
+            cursor.insert(marble);
        }
-        currentPlayer = (currentPlayer + 1) % numPlayers;
-    });
+    };
 
     let now = now.elapsed();
     println!("{}.{}s", now.as_secs(), now.subsec_millis());
